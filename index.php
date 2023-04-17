4d75c51b4efd33d0a8ad033416bae00e45225bfb
Mussavir Shahid <shahid.muhammad@student.greenriver.com>
2023-04-17 13:09:19 -0400

    ok

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once ('vendor/autoload.php');
+
+// Create an F3 (Fat Free Framework) object
+$f3 = Base::instance();
+
+$f3->route('GET /', function (){
+    echo "Hello, World";
+});
+
+// Run Fat Free Framework
+$f3->run();
